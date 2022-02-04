a13651a0e0fa1fd5fbfc70bf61e0f6ca92f38049
PhilippKuhnhardt <56841270+PhilippKuhnhardt@users.noreply.github.com>
2022-02-04 14:51:26 +0100

    Created user object

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -5,18 +6,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
-# In a real application the user and the hashed password would obviously be stored in a DB
-users = {
-    "Philipp": generate_password_hash("password"),
-    "Éric": generate_password_hash("strong_password")
-}
+class User:
+  def __init__(self, name, password_hash, model_access):
+    self.name = name
+    self.password_hash = password_hash
+    self.model_access = model_access
+
+# In a real application the user would be dynamically generated and store in a db
+users = [User("Philipp", generate_password_hash("password"), [1, 2]), User("Éric", generate_password_hash("strong_password"), [3])]
 
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and check_password_hash(users.get(username), password):
-        return username
+    return next((user for user in users if user.name == user.name and check_password_hash(user.password_hash, password)), False)
+
+@auth.get_user_roles
+def get_user_roles(user: User):
+    return user.model_access
 
 @app.route('/')
 @auth.login_required
 def index():
-    return "Hello, {}!".format(auth.current_user())
+    user: User = auth.current_user()
+    return "Hello, {}. Models you have access to: {}!".format(user.name, ", ".join(str(x) for x in user.model_access))
